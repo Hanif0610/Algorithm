719d5737d12ee71301fadfa017a73a83a4811253
Hanif <marbling1293@naver.com>
2020-08-14 08:27:59 +0900

    [ADD] Level 1

---
+++ Level1/약수의합/약수의합.java
@@ -0,0 +1,18 @@
+package Level1.약수의합;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i <= n; i++) {
+            if(n%i == 0) answer += i;
+        }
+        return answer;
+    }
+}
+
+public class 약수의합 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(12));
+    }
+}

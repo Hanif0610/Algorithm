4fe42ce0c3f9b10bf9be70fc9083d348c14c0fe6
배현호 <610hanif@gmail.com>
2021-09-15 16:45:29 +0900

    [ADD] Level 1

---
+++ Level1/문자열정수바꾸기/문자열정수로바꾸기.kt
@@ -0,0 +1,12 @@
+package Level1.문자열정수바꾸기
+
+class Solution {
+    fun solution(s: String): Int {
+        return s.toInt()
+    }
+}
+
+fun main(args: Array<String>) {
+    val solution = Solution()
+    println(solution.solution("123"))
+}

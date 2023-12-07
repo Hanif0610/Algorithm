7edfa423caa3aff97b3109cd20a3834238c63618
HyeonHo Bae <marbling1293@naver.com>
2023-12-07 23:30:46 +0900

    [ADD] 코딩테스트 입문 문제

---
+++ 코딩테스트입문/양꼬치/양꼬치.kt
@@ -0,0 +1,10 @@
+package 코딩테스트입문.양꼬치
+
+class Solution {
+    fun solution(n: Int, k: Int): Int = (n * 12_000) + (k * 2_000) - (n / 10 * 2_000)
+}
+
+fun main() {
+    var s = Solution()
+    println(s.solution(64, 6))
+}

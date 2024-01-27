22ea7468034d34a4d5a1ecaaae6fdb079789ec87
Hanif0610 <marbling1293@naver.com>
2024-01-27 15:13:02 +0900

    [ADD] Java 런타임을 위해 코드 주석

--- Level1/자릿수더하기/Solution.java
+++ Level1/자릿수더하기/Solution.java
@@ -1,14 +1,14 @@
-package Level1.자릿수더하기;
-
-public class Solution {
-    public int solution(int n) {
-        int answer = 0;
-
-        while(n > 0) {
-            answer += n%10;
-            n /= 10;
-        }
-
-        return answer;
-    }
-}
+//package Level1.자릿수더하기;
+//
+//public class Solution {
+//    public int solution(int n) {
+//        int answer = 0;
+//
+//        while(n > 0) {
+//            answer += n%10;
+//            n /= 10;
+//        }
+//
+//        return answer;
+//    }
+//}
